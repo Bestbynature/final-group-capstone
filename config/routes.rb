3e48dcc91d8f49124dcb91d36fb33e174e07dbed
MarwanDev <marwaneg@aucegypt.edu>
2023-08-18 17:11:37 +0300

    Add Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :reserved_flights
   resources :packages
   resources :flights
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root 'root#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'users#splash', as: :unauthenticated_root
+  end
 
   # Defines the root path route ("/")
   root "root#index"
