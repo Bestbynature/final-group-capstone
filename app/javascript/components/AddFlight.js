1d73f67bae62e52b13db318c3f1b6743206c8f9a
MarwanDev <marwaneg@aucegypt.edu>
2023-08-22 23:03:32 +0300

    Add packages to flights

--- app/javascript/components/AddFlight.js
+++ app/javascript/components/AddFlight.js
@@ -1,82 +1,148 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { postFlight, setPicture, setAvailableSlots, setBasePrice, setName, postFlights } from "../redux/flights/flightsSlice";
-import { useNavigate } from 'react-router-dom';
+import {
+  postFlight,
+  setPicture,
+  setAvailableSlots,
+  setBasePrice,
+  setName,
+  postFlights,
+} from "../redux/flights/flightsSlice";
+import { useNavigate } from "react-router-dom";
+import { fetchPackages } from "../redux/flights/flightsSlice";
 
 const AddFlight = () => {
-
   const dispatch = useDispatch();
   const history = useNavigate();
+  useEffect(() => {
+    dispatch(fetchPackages());
+  }, [dispatch]);
 
-  const { name, picture, basePrice, availableSlots, user_id } = useSelector((store) => store.flights);
+  const { packages } = useSelector((store) => store.flights);
+
+  const { name, picture, basePrice, availableSlots, user_id } = useSelector(
+    (store) => store.flights
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const formData = new FormData();
-        formData.append('flight[name]', name);
-        formData.append('flight[picture]', picture);
-        formData.append('flight[base_price]', basePrice);
-        formData.append('flight[available_slots]', availableSlots);
-        formData.append('flight[reserved]', 'false');
-        formData.append('flight[user_id]', user_id);
-       
-        dispatch(postFlight(formData));
-        history('/');
+    formData.append("flight[name]", name);
+    formData.append("flight[picture]", picture);
+    formData.append("flight[base_price]", basePrice);
+    formData.append("flight[available_slots]", availableSlots);
+    formData.append("flight[reserved]", "false");
+    formData.append("flight[user_id]", user_id);
+    selectedPackages.forEach((pkgId) =>
+      formData.append("flight[packages][]", pkgId)
+    );
+
+    dispatch(postFlight(formData));
+    history("/");
   };
 
   return (
-
-    <div className="modal fade" id="addModal" tabIndex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
+    <div
+      className="modal fade"
+      id="addModal"
+      tabIndex="-1"
+      aria-labelledby="addModalLabel"
+      aria-hidden="true"
+    >
       <form onSubmit={handleSubmit}>
         <div className="modal-dialog">
-            <div className="modal-content">
+          <div className="modal-content">
             <div className="modal-header">
-                <h1 className="modal-title" id="addModalLabel">Add a New Flight</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h1 className="modal-title" id="addModalLabel">
+                Add a New Flight
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
             <div className="modal-body">
-            <input className="form-control width-full mt-3"
-                    type="text"
-                    value={name}
-                    placeholder="Flight Name"
-                    onChange={(e) => dispatch(setName(e.target.value))}
-                    required
-                  />
+              <input
+                className="form-control width-full mt-3"
+                type="text"
+                value={name}
+                placeholder="Flight Name"
+                onChange={(e) => dispatch(setName(e.target.value))}
+                required
+              />
 
-                  <input className="form-control mt-3"
-                    type="text"
-                    value={picture}
-                    placeholder="Picture URL for the flight"
-                    onChange={(e) => dispatch(setPicture(e.target.value))}
-                    required
-                  />
+              <input
+                className="form-control mt-3"
+                type="text"
+                value={picture}
+                placeholder="Picture URL for the flight"
+                onChange={(e) => dispatch(setPicture(e.target.value))}
+                required
+              />
 
-                  <input className="form-control mt-3"
-                    type="number"
-                    value={basePrice}
-                    placeholder="Base Price for the destination"
-                    onChange={(e) => dispatch(setBasePrice(e.target.value))}
-                    required
-                  />
+              <input
+                className="form-control mt-3"
+                type="number"
+                value={basePrice}
+                placeholder="Base Price for the destination"
+                onChange={(e) => dispatch(setBasePrice(e.target.value))}
+                required
+              />
 
-                  <input className="form-control mt-3"
-                    type="number"
-                    value={availableSlots}
-                    placeholder="Available Slots for the destination"
-                    onChange={(e) => dispatch(setAvailableSlots(e.target.value))}
-                    required
+              <input
+                className="form-control mt-3"
+                type="number"
+                value={availableSlots}
+                placeholder="Available Slots for the destination"
+                onChange={(e) => dispatch(setAvailableSlots(e.target.value))}
+                required
+              />
+
+              {packages.map((pkg) => (
+                <div key={pkg.id} className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value={pkg.id}
+                    onChange={(e) => {
+                      const isChecked = e.target.checked;
+                      dispatch(
+                        setPackages((prevPackages) =>
+                          isChecked
+                            ? [...prevPackages, pkg.id]
+                            : prevPackages.filter((p) => p !== pkg.id)
+                        )
+                      );
+                    }}
                   />
+                  <label className="form-check-label">{pkg.name}</label>
+                </div>
+              ))}
             </div>
             <div className="modal-footer">
-                <button type="button" className="btn btn-danger form-control mt-2" data-bs-dismiss="modal">Cancel</button>
-                <button type="submit" className="btn add-submit form-control mt-3" data-bs-dismiss="modal" >Create a Flight</button>
+              <button
+                type="button"
+                className="btn btn-danger form-control mt-2"
+                data-bs-dismiss="modal"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="btn add-submit form-control mt-3"
+                data-bs-dismiss="modal"
+              >
+                Create a Flight
+              </button>
             </div>
-            </div>
+          </div>
         </div>
       </form>
     </div>
   );
-}
+};
 
 export default AddFlight;
--- app/javascript/redux/flights/flightsSlice.js
+++ app/javascript/redux/flights/flightsSlice.js
@@ -1,131 +1,166 @@
-import { createSlice, createAsyncThunk, isRejectedWithValue } from '@reduxjs/toolkit';
-import axios from 'axios';
+import {
+  createSlice,
+  createAsyncThunk,
+  isRejectedWithValue,
+} from "@reduxjs/toolkit";
+import axios from "axios";
 
-const url = 'http://localhost:3000/api/v1/flights';
+const url = "http://localhost:3000/api/v1/flights";
 
 const cities = [
-  { name: 'New York City', country: 'USA' },
-  { name: 'Tokyo', country: 'Japan' },
-  { name: 'London', country: 'UK' },
-  { name: 'Paris', country: 'France' },
-  { name: 'Los Angeles', country: 'USA' },
-  { name: 'Seoul', country: 'South Korea' },
-  { name: 'Shanghai', country: 'China' },
-  { name: 'Beijing', country: 'China' },
-  { name: 'Chicago', country: 'USA' },
-  { name: 'Istanbul', country: 'Turkey' },
-  { name: 'Miami', country: 'USA' },
-  { name: 'Moscow', country: 'Russia' },
-  { name: 'Sao Paulo', country: 'Brazil' },
-  { name: 'Mumbai', country: 'India' },
-  { name: 'Sydney', country: 'Australia' },
-  { name: 'Rio de Janeiro', country: 'Brazil' },
-  { name: 'Cairo', country: 'Egypt' },
-  { name: 'Mexico City', country: 'Mexico' },
-  { name: 'Bangkok', country: 'Thailand' },
-  { name: 'Toronto', country: 'Canada' },
-  { name: 'Buenos Aires', country: 'Argentina' },
-  { name: 'Dubai', country: 'UAE' },
-  { name: 'Hong Kong', country: 'China' },
-  { name: 'Singapore', country: 'Singapore' },
-  { name: 'Rome', country: 'Italy' },
-  { name: 'Berlin', country: 'Germany' },
-  { name: 'Barcelona', country: 'Spain' },
-  { name: 'Kuala Lumpur', country: 'Malaysia' },
-  { name: 'Amsterdam', country: 'Netherlands' },
-  { name: 'San Francisco', country: 'USA' },
-  { name: 'Vancouver', country: 'Canada' },
-  { name: 'San Diego', country: 'USA' },
-  { name: 'Houston', country: 'USA' },
-  { name: 'Washington, D.C.', country: 'USA' },
-  { name: 'Dallas', country: 'USA' },
-  { name: 'Istanbul', country: 'Turkey' },
-  { name: 'Abu Dhabi', country: 'UAE' },
-  { name: 'Jakarta', country: 'Indonesia' },
-  { name: 'Boston', country: 'USA' },
-  { name: 'Atlanta', country: 'USA' },
-  { name: 'Seattle', country: 'USA' },
-  { name: 'Tel Aviv', country: 'Israel' },
-  { name: 'Johannesburg', country: 'South Africa' },
-  { name: 'Melbourne', country: 'Australia' },
-  { name: 'Dubai', country: 'UAE' },
-  { name: 'Munich', country: 'Germany' },
-  { name: 'Montreal', country: 'Canada' },
-  { name: 'Prague', country: 'Czech Republic' },
-  { name: 'Auckland', country: 'New Zealand' },
-  { name: 'Stockholm', country: 'Sweden' },
+  { name: "New York City", country: "USA" },
+  { name: "Tokyo", country: "Japan" },
+  { name: "London", country: "UK" },
+  { name: "Paris", country: "France" },
+  { name: "Los Angeles", country: "USA" },
+  { name: "Seoul", country: "South Korea" },
+  { name: "Shanghai", country: "China" },
+  { name: "Beijing", country: "China" },
+  { name: "Chicago", country: "USA" },
+  { name: "Istanbul", country: "Turkey" },
+  { name: "Miami", country: "USA" },
+  { name: "Moscow", country: "Russia" },
+  { name: "Sao Paulo", country: "Brazil" },
+  { name: "Mumbai", country: "India" },
+  { name: "Sydney", country: "Australia" },
+  { name: "Rio de Janeiro", country: "Brazil" },
+  { name: "Cairo", country: "Egypt" },
+  { name: "Mexico City", country: "Mexico" },
+  { name: "Bangkok", country: "Thailand" },
+  { name: "Toronto", country: "Canada" },
+  { name: "Buenos Aires", country: "Argentina" },
+  { name: "Dubai", country: "UAE" },
+  { name: "Hong Kong", country: "China" },
+  { name: "Singapore", country: "Singapore" },
+  { name: "Rome", country: "Italy" },
+  { name: "Berlin", country: "Germany" },
+  { name: "Barcelona", country: "Spain" },
+  { name: "Kuala Lumpur", country: "Malaysia" },
+  { name: "Amsterdam", country: "Netherlands" },
+  { name: "San Francisco", country: "USA" },
+  { name: "Vancouver", country: "Canada" },
+  { name: "San Diego", country: "USA" },
+  { name: "Houston", country: "USA" },
+  { name: "Washington, D.C.", country: "USA" },
+  { name: "Dallas", country: "USA" },
+  { name: "Istanbul", country: "Turkey" },
+  { name: "Abu Dhabi", country: "UAE" },
+  { name: "Jakarta", country: "Indonesia" },
+  { name: "Boston", country: "USA" },
+  { name: "Atlanta", country: "USA" },
+  { name: "Seattle", country: "USA" },
+  { name: "Tel Aviv", country: "Israel" },
+  { name: "Johannesburg", country: "South Africa" },
+  { name: "Melbourne", country: "Australia" },
+  { name: "Dubai", country: "UAE" },
+  { name: "Munich", country: "Germany" },
+  { name: "Montreal", country: "Canada" },
+  { name: "Prague", country: "Czech Republic" },
+  { name: "Auckland", country: "New Zealand" },
+  { name: "Stockholm", country: "Sweden" },
 ];
 
-export const fetchFlights = createAsyncThunk('flights/fetchFlights', async () => {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    return isRejectedWithValue(error.response.data);
+export const fetchFlights = createAsyncThunk(
+  "flights/fetchFlights",
+  async () => {
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      return isRejectedWithValue(error.response.data);
+    }
   }
-});
+);
 
-export const deleteFlight = createAsyncThunk('flights/deleteFlight', async (flightId) => {
-  try {
-    await axios.delete(`${url}/${flightId}`);
-    return flightId;
-  } catch (error) {
-    return isRejectedWithValue(error.response.data);
+export const fetchPackages = createAsyncThunk(
+  "packages/fetchPackages",
+  async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v1/packages");
+      return response.data;
+    } catch (error) {
+      return isRejectedWithValue(error.response.data);
+    }
   }
-});
+);
 
-export const fetchFlightDetails = createAsyncThunk('flights/fetchFlightDetails', async (id) => {
-  try {
-    const response = await axios.get(`${url}/${id}`);
-    return response.data;
-  } catch (error) {
-    return isRejectedWithValue(error.response.data);
+export const deleteFlight = createAsyncThunk(
+  "flights/deleteFlight",
+  async (flightId) => {
+    try {
+      await axios.delete(`${url}/${flightId}`);
+      return flightId;
+    } catch (error) {
+      return isRejectedWithValue(error.response.data);
+    }
   }
-});
+);
 
-export const postFlight = createAsyncThunk('flights/postFlights', async (formData) => {
-  try {
-    const response = await axios.post(url, formData);
-    return response.data;
-  } catch (error) {
-    return isRejectedWithValue(error.response.data);
+export const fetchFlightDetails = createAsyncThunk(
+  "flights/fetchFlightDetails",
+  async (id) => {
+    try {
+      const response = await axios.get(`${url}/${id}`);
+      return response.data;
+    } catch (error) {
+      return isRejectedWithValue(error.response.data);
+    }
   }
-});
+);
+
+export const postFlight = createAsyncThunk(
+  "flights/postFlights",
+  async (formData) => {
+    try {
+      const response = await axios.post(url, formData);
+      return response.data;
+    } catch (error) {
+      return isRejectedWithValue(error.response.data);
+    }
+  }
+);
 
 const initialState = {
   flights: [],
   flightDetails: {},
   cities,
-  user: '',
+  user: "",
   user_id: 0,
-  status: 'idle',
+  status: "idle",
   error: null,
   cwidth: 0,
-  active: '',
-  city: '',
-  date: '',
-  flight: '',
+  active: "",
+  city: "",
+  date: "",
+  flight: "",
   reservedFlights: [],
-  name: '',
-  picture: '',
+  name: "",
+  picture: "",
   basePrice: null,
   availableSlots: null,
+  packages: [],
 };
 
 const flightsSlice = createSlice({
-  name: 'flights',
+  name: "flights",
   initialState,
   reducers: {
     setcwidth(state, action) {
-      if (action.payload === 'left' || action.payload === 'right') {
+      if (action.payload === "left" || action.payload === "right") {
         return { ...state, active: action.payload };
       } else {
         return { ...state, cwidth: action.payload };
       }
     },
     setReservedFlights(state, action) {
-      localStorage.setItem('reservedFlights', JSON.stringify([...state.reservedFlights, action.payload]));
-      return { ...state, reservedFlights: [...state.reservedFlights, action.payload] };
+      localStorage.setItem(
+        "reservedFlights",
+        JSON.stringify([...state.reservedFlights, action.payload])
+      );
+      return {
+        ...state,
+        reservedFlights: [...state.reservedFlights, action.payload],
+      };
     },
     setCity(state, action) {
       return { ...state, city: action.payload };
@@ -152,33 +187,56 @@ const flightsSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchFlights.pending, (state) => {
-        return { ...state, status: 'loading' };
+        return { ...state, status: "loading" };
       })
       .addCase(fetchFlights.fulfilled, (state, action) => {
-        return { ...state, flights: [...action.payload.flights], status: 'succeeded', user: action.payload.user.name, user_id: action.payload.user.id };
+        return {
+          ...state,
+          flights: [...action.payload.flights],
+          status: "succeeded",
+          user: action.payload.user.name,
+          user_id: action.payload.user.id,
+        };
       })
       .addCase(fetchFlights.rejected, (state, action) => {
-        return { ...state, status: 'failed', error: action.payload };
+        return { ...state, status: "failed", error: action.payload };
+      })
+      .addCase(fetchPackages.pending, (state) => {
+        return { ...state, status: "loading" };
+      })
+      .addCase(fetchPackages.fulfilled, (state, action) => {
+        state.packages = action.payload;
+        console.log(state.packages);
+      })
+      .addCase(fetchPackages.rejected, (state, action) => {
+        return { ...state, status: "failed", error: action.payload };
       })
       .addCase(postFlight.pending, (state) => {
-        return { ...state, status: 'loading' };
+        return { ...state, status: "loading" };
       })
       .addCase(postFlight.fulfilled, (state, action) => {
-        return { ...state, flights: [...action.payload.flights], status: 'succeeded', user: action.payload.user };
+        return {
+          ...state,
+          flights: [...action.payload.flights],
+          status: "succeeded",
+          user: action.payload.user,
+        };
       })
       .addCase(postFlight.rejected, (state, action) => {
-        return { ...state, status: 'failed', error: action.payload };
+        return { ...state, status: "failed", error: action.payload };
       })
       .addCase(deleteFlight.pending, (state) => {
-        return { ...state, status: 'loading' };
+        return { ...state, status: "loading" };
       })
       .addCase(deleteFlight.fulfilled, (state, action) => {
         const deletedFlightId = action.payload;
-        const updatedFlights = state.flights.filter((flight) => flight.id !== deletedFlightId);
+        const updatedFlights = state.flights.filter(
+          (flight) => flight.id !== deletedFlightId
+        );
         return { ...state, flights: updatedFlights };
       })
       .addCase(deleteFlight.rejected, (state, action) => {
-        return { ...state, status: 'failed', error: action.payload };
+        return { ...state, status: "failed", error: action.payload };
       })
       .addCase(fetchFlightDetails.pending, (state) => {
         return { ...state, loading: true };
@@ -195,6 +253,18 @@ const flightsSlice = createSlice({
   },
 });
 
-export const { setcwidth, setCsrfToken, addFlight, setCity, setDate, setFlight, setReservedFlights, setName, setAvailableSlots, setBasePrice, setPicture } = flightsSlice.actions;
+export const {
+  setcwidth,
+  setCsrfToken,
+  addFlight,
+  setCity,
+  setDate,
+  setFlight,
+  setReservedFlights,
+  setName,
+  setAvailableSlots,
+  setBasePrice,
+  setPicture,
+} = flightsSlice.actions;
 
 export default flightsSlice.reducer;
