8e569a0ec2e68d42b9829cf0e43385ac7a1c1b88
MarwanDev <marwaneg@aucegypt.edu>
2023-08-23 17:56:10 +0300

    Add POST flights

--- doc/api/v1/flights_controller.rb
+++ doc/api/v1/flights_controller.rb
@@ -4,4 +4,12 @@ module Api::V1::FlightsControllerDoc
   api :GET, '/v1/flights', 'get flights'
   def index; end
 
+  api :POST, '/v1/flights', 'create new flight'
+  param :flight, Hash do
+    param :name, String
+    param :base_price, String
+    param :picture, String
+    param :available_slots, String
+  end
+  def create; end
 end
