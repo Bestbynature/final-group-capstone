986e44ec6aa43ccfaa066366f085f35fb0ef4be6
MarwanDev <marwaneg@aucegypt.edu>
2023-08-23 18:49:27 +0300

    Generate swagger JSON files

--- db/migrate/20230823094650_remove_packages_table.rb
+++ db/migrate/20230823094650_remove_packages_table.rb
@@ -1,6 +1,5 @@
 class RemovePackagesTable < ActiveRecord::Migration[7.0]
   def change
-    remove_column :flights, :packages, :string
     remove_column :reserved_flights, :package_id, :integer
     drop_table :packages
   end
--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_094650) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -47,7 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_094650) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
