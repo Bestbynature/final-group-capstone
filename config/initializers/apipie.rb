52c7e773b491708689a3d5e7d5a3c29ebaa94826
MarwanDev <marwaneg@aucegypt.edu>
2023-08-23 17:07:50 +0300

    Configure apipie

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -3,5 +3,13 @@ Apipie.configure do |config|
   config.api_base_url            = "/api"
   config.doc_base_url            = "/apipie"
   # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.show_all_examples       = true
+  config.translate               = false
+  config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
+
+  config.authenticate = proc do
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['APIPIE_USERNAME'] && password == ENV['APIPIE_PASSWORD']
+    end
+  end
 end
