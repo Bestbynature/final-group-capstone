0158f5adccc14b3fe871c07ed3a66e29506d2ac7
MarwanDev <marwaneg@aucegypt.edu>
2023-08-23 17:09:35 +0300

    Initiate rspec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'support/request_spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+require 'support/apipie_helper'
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.include RequestSpecHelper, type: :request
+end
