9ea3dd20e3501c4d1a76b00ade52654854050352
MarwanDev <marwaneg@aucegypt.edu>
2023-08-25 20:22:51 +0300

    Add flights test

---
+++ src/tests/flights.test.js
@@ -0,0 +1,21 @@
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import Flights from '../../app/javascript/components/Flights';
+import store from '../../app/javascript/redux/store';
+
+describe('Flights', () => {
+  it('renders flights correctly', () => {
+    const details = renderer
+      .create(
+        <BrowserRouter>
+          <Provider store={store}>
+            <Flights />
+          </Provider>
+        </BrowserRouter>,
+      )
+      .toJSON();
+    expect(details).toMatchSnapshot();
+  });
+});
