e661af70a558267b9e59c798249164ab631d3d72
MarwanDev <marwaneg@aucegypt.edu>
2023-08-26 12:22:19 +0300

    Add flights test

--- src/tests/flights.test.js
+++ src/tests/flights.test.js
@@ -34,12 +34,20 @@ describe('Flights', () => {
 
 describe('redux', () => {
   const state = {
-    flights: [],
+    flights: [
+      {
+        name: 'Egypt', picture: 'https://images.pexels.com/photos/3958516/pexels-photo-3958516.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=300', base_price: 1100, available_slots: 10, reserved: false,
+      },
+    ],
     isLoading: false,
     error: null,
   };
   expect(reducer(state, fetchFlights)).toEqual({
-    flights: [],
+    flights: [
+      {
+        name: 'Egypt', picture: 'https://images.pexels.com/photos/3958516/pexels-photo-3958516.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=300', base_price: 1100, available_slots: 10, reserved: false,
+      },
+    ],
     isLoading: false,
     error: null,
   });
